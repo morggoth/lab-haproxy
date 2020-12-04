0f4b79e506a38cadb9dd3f6ddf608815c08e7b55
Konstantin Yakovlev <morggoth70@gmail.com>
2020-12-05 00:50:56 +0300

    Change static sites for simple go web app

---
+++ app/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, "Hello from %s\n", hostname)
+}
